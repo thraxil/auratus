a2c243a5db6a14b3760bd6bc4d6d12a338705bb4
Anders Pearson <anders@columbia.edu>
2012-07-31 15:03:28 -0400

    split out main index pages

--- auratus/main/views.py
+++ auratus/main/views.py
@@ -3,8 +3,14 @@ from auratus.main.models import Photo, Album, Tag
 
 @render_to('main/index.html')
 def index(request):
-    return dict(
-        photos=Photo.objects.all()[:50],
-        albums=Album.objects.all(),
-        tags=Tag.objects.all(),
-        )
+    return dict(photos=Photo.objects.all()[:50])
+
+
+@render_to('main/albums.html')
+def albums(request):
+    return dict(albums=Album.objects.all())
+
+
+@render_to('main/tags.html')
+def tags(request):
+    return dict(tags=Tag.objects.all())
