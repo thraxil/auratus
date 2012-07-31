81ba78917f18dc183311ece4e700e81540e8063a
Anders Pearson <anders@columbia.edu>
2012-07-31 16:04:17 -0400

    photo detail page

--- auratus/main/views.py
+++ auratus/main/views.py
@@ -1,5 +1,6 @@
 from annoying.decorators import render_to
 from auratus.main.models import Photo, Album, Tag
+from django.shortcuts import get_object_or_404
 
 @render_to('main/index.html')
 def index(request):
@@ -14,3 +15,9 @@ def albums(request):
 @render_to('main/tags.html')
 def tags(request):
     return dict(tags=Tag.objects.all())
+
+
+@render_to('main/photo.html')
+def photo(request, id):
+    p = get_object_or_404(Photo, pk=id)
+    return dict(photo=p)
