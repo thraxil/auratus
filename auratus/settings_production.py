b8c644c0c9da9ff7e2276a6119e993744788d735
Anders Pearson <anders@columbia.edu>
2015-03-01 16:07:34 +0100

    cloudfront

--- auratus/settings_production.py
+++ auratus/settings_production.py
@@ -20,12 +20,15 @@ TEMPLATE_DEBUG = DEBUG
 
 STATICFILES_DIRS = ()
 
+AWS_S3_CUSTOM_DOMAIN = "d12khfts9rqjej.cloudfront.net"
+AWS_IS_GZIPPED = True
+
 AWS_STORAGE_BUCKET_NAME = "thraxil-auratus-static-prod"
 AWS_PRELOAD_METADATA = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
-S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_ROOT = STATIC_ROOT
