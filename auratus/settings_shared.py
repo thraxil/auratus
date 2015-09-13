251cf4fcf8e9ec4c9e3c6fc1ea0808c95ac37a2d
Anders Pearson <anders@columbia.edu>
2015-09-13 14:56:36 +0200

    convert to thraxilsettings

--- auratus/settings_shared.py
+++ auratus/settings_shared.py
@@ -1,147 +1,18 @@
 # Django settings for auratus project.
 import os.path
-import sys
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+from thraxilsettings.shared import common
 
-ADMINS = ('anders@columbia.edu', )
+base = os.path.dirname(__file__)
+locals().update(common(app='auratus', base=base))
 
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'auratus',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-        }
-    }
-
-USE_TZ = True
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/auratus/uploads/"
-MEDIA_URL = '/uploads/'
-SECRET_KEY = '3c328c05-b6c6-43da-a2db-9dab5b25383b'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-)
-
-ROOT_URLCONF = 'auratus.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/auratus/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django_statsd',
+INSTALLED_APPS += [  # noqa
     'bootstrapform',
     'auratus.main',
-    'gunicorn',
-    'django_markwhat',
-    'compressor',
-    'storages',
-    'smoketest',
 ]
 
-LETTUCE_APPS = (
-    'auratus.main',
-)
-
-STATIC_URL = "/media/"
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATIC_ROOT = ""
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'auratus'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-if 'test' in sys.argv:
-    DATABASE_ENGINE = 'sqlite3'
-SOUTH_TESTS_MIGRATE = False
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[auratus] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "auratus@thraxil.org"
-DEFAULT_FROM_EMAIL = SERVER_EMAIL
-
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', 'sitemedia'),
-)
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
+ALLOWED_HOSTS += ['.thraxil.org']  # noqa
 
 RETICULUM_BASE = "http://behemoth.ccnmtl.columbia.edu:14001/"
 
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
-
-ALLOWED_HOSTS = ['localhost', '.thraxil.org']
