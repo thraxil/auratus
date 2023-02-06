905a51d65fd78f9b5c60515aee2c7b801c6b9850
Anders Pearson <anders@thraxil.org>
2023-02-06 14:08:49 +0000

    django 4.1.6

--- auratus/urls.py
+++ auratus/urls.py
@@ -1,8 +1,8 @@
 import django.views.static
 from django.conf import settings
-from django.conf.urls import include, re_path, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.urls import include, re_path
 from django.views.generic import TemplateView
 
 from .main.views import (AddAlbum, AddPhoto, AlbumSlideshow, AlbumsView,
@@ -14,37 +14,37 @@ admin.autodiscover()
 
 urlpatterns = [
     re_path(r"^accounts/", include("django.contrib.auth.urls")),
-    url(r"^$", Index.as_view(), name="index"),
-    url(r"smoketest/", include("smoketest.urls")),
-    url(r"^page/(?P<page>\d+)/$", Index.as_view(), name="page"),
-    url(r"^photo/(?P<pk>\d+)/$", PhotoView.as_view(), name="photo"),
-    url(r"^album/$", AlbumsView.as_view(), name="album-index"),
-    url(r"^album/(?P<pk>\d+)/$", AlbumView.as_view(), name="album"),
-    url(
+    re_path(r"^$", Index.as_view(), name="index"),
+    re_path(r"smoketest/", include("smoketest.urls")),
+    re_path(r"^page/(?P<page>\d+)/$", Index.as_view(), name="page"),
+    re_path(r"^photo/(?P<pk>\d+)/$", PhotoView.as_view(), name="photo"),
+    re_path(r"^album/$", AlbumsView.as_view(), name="album-index"),
+    re_path(r"^album/(?P<pk>\d+)/$", AlbumView.as_view(), name="album"),
+    re_path(
         r"^album/(?P<id>\d+)/add_photo/$",
         view=login_required(AddPhoto.as_view()),
         name="add-photo",
     ),
-    url(
+    re_path(
         r"^album/(?P<id>\d+)/bulk/(?P<token>[^\/]+)/$",
         BulkAddPhotos.as_view(),
         name="bulk-add",
     ),
-    url(
+    re_path(
         r"^album/(?P<pk>\d+)/slideshow/$",
         AlbumSlideshow.as_view(),
         name="album-slideshow",
     ),
-    url(r"^tag/$", TagsView.as_view(), name="tag-index"),
-    url(r"^tag/(?P<slug>\w+)/$", TagView.as_view(), name="tag"),
-    url(
+    re_path(r"^tag/$", TagsView.as_view(), name="tag-index"),
+    re_path(r"^tag/(?P<slug>\w+)/$", TagView.as_view(), name="tag"),
+    re_path(
         r"^add_album/$",
         view=login_required(AddAlbum.as_view()),
         name="add-album",
     ),
-    url(r"^admin/", admin.site.urls),
-    url(r"^stats/", TemplateView.as_view(template_name="stats.html")),
-    url(
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^stats/", TemplateView.as_view(template_name="stats.html")),
+    re_path(
         r"^uploads/(?P<path>.*)$",
         django.views.static.serve,
         {"document_root": settings.MEDIA_ROOT},
@@ -55,5 +55,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        url(r"^__debug__/", include(debug_toolbar.urls)),
+        re_path(r"^__debug__/", include(debug_toolbar.urls)),
     ]
